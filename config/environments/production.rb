a28ce4815643531dffc5bfbd8542736be5af4177
Sean <sean@aokstudio.com>
2013-10-28 23:34:24 +0000

    add s3 for paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Pins::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  #sets paperclip to upload to amazon s3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
+
 end
