f3f876ccb679d31c28e3f9fc1ca4dd238eb6e69a
Sean Mongey <sm@smongey.com>
2013-12-02 19:35:34 +0000

    validation et al

--- Gemfile
+++ Gemfile
@@ -18,6 +18,9 @@ gem 'devise', '~> 3.1.1'
 gem 'paperclip', '~> 3.0'
 gem 'aws-sdk', '~> 1.20.0'
 gem 'masonry-rails', '~> 0.2.0'
+gem 'will_paginate', '~> 3.0.5'
+gem 'rb-fsevent'
+gem 'guard-livereload'
 
 group :development, :test do
 	gem 'sqlite3'
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,7 +3,7 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
 
   def index
-    @pins = Pin.all.order("created_at DESC")
+    @pins = Pin.all.order("updated_at DESC").paginate(:page => params[:page], :per_page => 2)
   end
 
   def show
